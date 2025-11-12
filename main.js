98619a29bd9e03c12bb0ca49b6b784608ea9dfe2
Tomoki <sktar7214@gmail.com>
2025-11-13 02:29:10 +0900

    create one square css and js

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-'use strict';
+"use strict";
 
+//create square 
+const container = document.querySelector(".container");
+const square = document.createElement("div");
+container.appendChild(square);
+square.classList.add("square")

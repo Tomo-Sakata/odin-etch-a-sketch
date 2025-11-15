a52742261faa47d47dd1932c4ffb9c8129fc30d9
Tomoki <sktar7214@gmail.com>
2025-11-15 17:10:49 +0900

    add newgrid event by user input and create mouseover event function
    -modify css style container and square width-height

--- main.js
+++ main.js
@@ -1,19 +1,43 @@
 "use strict";
 
-//create square 
+//create default square 
 const container = document.querySelector(".container");
-
-for(let i = 0; i < 100; i++){
+for(let i = 0; i < 256; i++){
 const square = document.createElement("div");
+square.style.width = (parseInt(getComputedStyle(container).width) / 16) + "px";
+square.style.height = (parseInt(getComputedStyle(container).height) / 16) + "px";
 square.classList.add("square");
 container.appendChild(square);
-console.log(container);
+}
+mouseoverEvent();
+
+
+// mouseover event
+function mouseoverEvent() {
+  const squareDivs = document.querySelectorAll(".square");
+  squareDivs.forEach(function (squareDiv){
+  squareDiv.addEventListener("mouseover",function () {
+  squareDiv.style.backgroundColor = "red";
+  })
+});
 }
 
-const squareDivs = document.querySelectorAll(".square");
-squareDivs.forEach(function (squareDiv){
-squareDiv.addEventListener('mouseover',function () {
-  squareDiv.style.backgroundColor = "red";
-})
-});
-
+//create newgrid by user input 
+const newGridDiv = document.querySelector(".newgrid");
+newGridDiv.addEventListener("click", function(){
+  const userInput = prompt("You can set the grid size. You can enter up to 100.");
+  const userInputNumber = parseInt(userInput);
+  if(userInputNumber >= 1 && userInputNumber <= 100){
+  container.innerHTML = "";
+    for(let i = 0; i < userInputNumber * userInputNumber; i++){
+      const newSquare = document.createElement("div");
+      newSquare.style.width = (parseInt(getComputedStyle(container).width) / userInputNumber) + "px";
+      newSquare.style.height = (parseInt(getComputedStyle(container).height) / userInputNumber) + "px";
+      newSquare.classList.add("square");
+      container.appendChild(newSquare);
+    }
+  }
+  else {alert("enter 1-100");
+  }
+  mouseoverEvent();
+  });

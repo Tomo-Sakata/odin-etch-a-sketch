91d8a2a02592ee3dc0e05a2e6d7b4b076bd619da
Tomoki <sktar7214@gmail.com>
2025-11-15 11:04:22 +0900

    add addEventListener mouseover chane background color

--- main.js
+++ main.js
@@ -2,6 +2,18 @@
 
 //create square 
 const container = document.querySelector(".container");
+
+for(let i = 0; i < 100; i++){
 const square = document.createElement("div");
+square.classList.add("square");
 container.appendChild(square);
-square.classList.add("square")
+console.log(container);
+}
+
+const squareDivs = document.querySelectorAll(".square");
+squareDivs.forEach(function (squareDiv){
+squareDiv.addEventListener('mouseover',function () {
+  squareDiv.style.backgroundColor = "red";
+})
+});
+
